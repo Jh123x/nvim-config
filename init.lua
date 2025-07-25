0ea269aefcff8188f17472e7bfe03f55d916e87a
Wen Junhua <wenjh1998@gmail.com>
2025-07-25 23:43:35 +0800

    feat: add initial nvim config

---
+++ init.lua
@@ -0,0 +1,59 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+-- Load Lazy Plugin
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "nvim-telescope/telescope.nvim", tag = "0.1.8",
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"}
+}
+
+local opts = {
+  checker = { enabled = true },
+}
+
+require("lazy").setup(plugins,opts)
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"python", "markdown", "go", "lua", "sql", "javascript", "typescript"},
+  sync_install = false,
+  auto_install = true,
+  highlight={ enable = true },
+  indent = { enable = true }
+})
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
+
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+
