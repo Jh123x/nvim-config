38a8c89224a47c4ab3dad4b4a4928d47b6896f8b
Wen Junhua <wenjh1998@gmail.com>
2025-07-26 00:21:53 +0800

    chore: refactor nvim config

---
+++ lua/vim-options.lua
@@ -0,0 +1,13 @@
+-- Tab formats
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+-- Set leader
+vim.g.mapleader = " "
+
+-- Line numbers
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+
