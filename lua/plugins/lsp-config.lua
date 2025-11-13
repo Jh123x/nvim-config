122c000d72f602723486fc3b3825bc561565c32a
Wen Junhua <wenjh1998@gmail.com>
2025-11-13 23:38:11 +0800

    chore: update nvim config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,39 +10,4 @@ return {
       require("mason").setup()
     end,
   },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      auto_install = true,
-    },
-    dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      "neovim/nvim-lspconfig",
-    },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "pyright",
-          "gopls",
-          "ts_ls",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.ts_ls.setup({})
-      lspconfig.pyright.setup({})
-      lspconfig.gopls.setup({})
-
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
 }
