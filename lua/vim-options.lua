e532e40f54c209701829ce1d7e13ac3a4d308cdb
Wen Junhua <wenjh1998@gmail.com>
2026-01-02 09:50:56 +0800

    feat: format buffer on save

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -14,3 +14,12 @@ vim.cmd("set relativenumber")
 -- Enable virtual warnings/errors
 vim.diagnostic.config({ virtual_text = true })
 
+-- Autocommand to format on save using LSP
+vim.api.nvim_create_autocmd("BufWritePre", {
+    group = vim.api.nvim_create_augroup("LspFormatting", { clear = true }),
+    callback = function(args)
+        vim.lsp.buf.format({ async = false, bufnr = args.buf })
+    end,
+})
+
+
