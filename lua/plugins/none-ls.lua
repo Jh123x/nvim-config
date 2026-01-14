687b95a5c88866f7ce5e35d1bf2184cc260850fa
Wen Junhua <wenjh1998@gmail.com>
2026-01-14 09:49:17 +0800

    feat: add auto format on save

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,15 +1,28 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.diagnostics.stylua,
-				null_ls.builtins.completion.spell,
-			},
-		})
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.diagnostics.stylua,
+        null_ls.builtins.completion.spell,
+      },
+    })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    -- Define the autocommand group
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup("NoneLsFormatting", { clear = true }),
+      callback = function(opts)
+        -- Check if a formatter is available for the current buffer and run it
+        vim.lsp.buf.format({
+          async = true,      -- Asynchronous formatting is generally preferred
+          bufnr = opts.buf,
+          timeout_ms = 1000, -- Adjust timeout as needed
+        })
+      end,
+    })
+  end,
 }
